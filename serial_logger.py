b99de4e1f0acf1f3ebb2a41f1b9c1031081eeaf6
Stefan Koch <Stefan-2.Koch@alcon.com>
2018-03-22 12:12:17 +0100

    add option to dump text on console only

--- serial_logger.py
+++ serial_logger.py
@@ -10,19 +10,38 @@ import time
 import os
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-parser.add_argument("-d", "--device", help="device to read from", default="/dev/ttyUSB0")
-parser.add_argument("-s", "--speed", help="speed in bps", default=9600, type=int)
+parser.add_argument("-d", "--device",  help="device to read from",  default="/dev/ttyUSB0")
+parser.add_argument("-s", "--speed",   help="speed in bps",         default=9600, type=int)
+parser.add_argument("-c", "--console", help="show only on console", action="store_true", dest="c")
+
 args = parser.parse_args()
 
-outputFilePath = os.path.join(os.path.dirname(__file__),
+if (not args.c):
+    outputFilePath = os.path.join(os.path.dirname(__file__),
                  datetime.datetime.now().strftime("%Y-%m-%dT%H.%M.%S") + ".bin")
+    outputFile = open(outputFilePath, mode='wb')
+    print("will log to " + outputFilePath)
+else:
+    print("will log only to console")
 
-with serial.Serial(args.device, args.speed) as ser, open(outputFilePath, mode='wb') as outputFile:
-    print("Logging started. Ctrl-C to stop.") 
-    try:
-        while True:
+try:
+    ser = serial.Serial(args.device, args.speed)
+except:
+    print("Could not open serial line")
+    exit()
+
+print("Logging started. Ctrl-C to stop.") 
+try:
+    while True:
+        if (args.c):
+            character = str(ser.read(1), encoding='utf-8')
+            print(character, end="", flush=True)
+        else:
             time.sleep(1)
             outputFile.write((ser.read(ser.inWaiting())))
             outputFile.flush()
-    except KeyboardInterrupt:
-        print("Logging stopped")
+
+except KeyboardInterrupt:
+    print("Logging stopped")
+    if (not args.c):
+        outputFile.close()
